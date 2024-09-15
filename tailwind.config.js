3ed80a727cec54f204447b004a5dc897e44e2935
Mahfooz-Siddiqui <mahfoozsiddiqui490@gmail.com>
2024-09-15 23:18:27 +0530

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
     extend: {
       keyframes: {
         changeImage: {
-          '0%': { 'background-image': 'url("/src/assets/building1.jpg")' },
-          '33.33%': { 'background-image': 'url("/src/assets/building2.jpg")' },
-          '66.66%': { 'background-image': 'url("/src/assets/building3.jpg")' },
-          '100%': { 'background-image': 'url("/src/assets/building1.jpg")' }
+          '0%': { 'background-image': 'url("./assets/building1.jpg")' },
+          '33.33%': { 'background-image': 'url("./assets/building2.jpg")' },
+          '66.66%': { 'background-image': 'url("./assets/building3.jpg")' },
+          '100%': { 'background-image': 'url("./assets/building1.jpg")' }
         },
       },
       animation: {
